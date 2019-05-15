4dd20aeff2394661ee7640bfb84feeea3f60a227
MacBook Pro <bruno.tamborero@est.fib.upc.edu>
2019-05-15 16:36:43 +0200

    New kernel

--- imfilter.cu
+++ imfilter.cu
@@ -33,6 +33,29 @@ __global__ void Kernel01(int size_filter, double *FilterMatrix, unsigned char* m
     }
 }
 
+__global__ void Kernel02(int size_filter, double *FilterMatrix, unsigned char* matrix_orig, int height, int width, unsigned char* matrix_filt) {
+
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = row;
+    int j = col;
+    if (row < height && col < width) {
+        float accumulator_red = 0;
+        float accumulator_green = 0;
+        float accumulator_blue = 0;
+        for (size_t k = 0; k < size_filter; k++) { // kernel rows
+            for (size_t l = 0; l < size_filter; l++) { // kernel elements/cols
+                accumulator_red += FilterMatrix[k*size_filter + l] * (unsigned int) (matrix_orig[((i+k-1)*width + (j+l-1))*size_filter + 0]);
+                accumulator_green += FilterMatrix[k*size_filter + l] * (unsigned int) (matrix_orig[((i+k-1)*width + (j+l-1))*size_filter + 1]);
+                accumulator_blue += FilterMatrix[k*size_filter + l] * (unsigned int) (matrix_orig[((i+k-1)*width + (j+l-1))*size_filter + 2]);
+            }
+        }
+        matrix_filt[(i*width + j)*size_filter + 0]= (unsigned int) accumulator_red;
+        matrix_filt[(i*width + j)*size_filter + 1] = (unsigned int) accumulator_green;
+        matrix_filt[(i*width + j)*size_filter + 2] = (unsigned int) accumulator_blue;
+    }
+}
+
 void seq(int size_filter, double *FilterMatrix, unsigned char* matrix_orig, int height, int width, unsigned char* matrix_filt) {
     for (size_t i = size_filter/2; i < height-size_filter/2; i++) { // image row
         for (size_t j = size_filter/2; j < width-size_filter/2; j++) { // pixels in image row
@@ -151,12 +174,72 @@ int main (int argc, char *argv[])
     cudaEventElapsedTime(&TiempoKernelCuda1, E1, E2);
     cudaEventDestroy(E0); cudaEventDestroy(E1); cudaEventDestroy(E2); cudaEventDestroy(E3);
 
+    // Cuda 2
+    float TiempoTotalCuda2, TiempoKernelCuda2;
+    cudaEvent_t E4, E5, E6, E7;
+    unsigned char *d_matrix_orig2, *d_matrix_filt2, *h_matrix_filt2;
+    h_matrix_filt2 = (unsigned char *) malloc (sizeof(unsigned char)*height*width*channels);
+    double *d_K2;
+
+
+    cudaEventCreate(&E4);
+    cudaEventCreate(&E5);
+    cudaEventCreate(&E6);
+    cudaEventCreate(&E7);
+
+    cudaEventRecord(E4, 0);
+    cudaEventSynchronize(E4);
+
+    // Obtener Memoria en el device
+    cudaMalloc((double**)&d_K2, numBytesK);
+    cudaMalloc((unsigned char ***)&d_matrix_orig2, numBytesOrig);
+    cudaMalloc((unsigned char ***)&d_matrix_filt2, numBytesFilt);
+
+    // Copiar datos desde el host en el device
+    cudaError err = cudaMemcpy(d_K2, h_K, numBytesK, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) {
+        printf("1: CUDA error copying to Device: %s\n", cudaGetErrorString(err));
+    }
+    err = cudaMemcpy(d_matrix_orig2, h_matrix_orig2, numBytesOrig, cudaMemcpyHostToDevice);
+    if (err!=cudaSuccess) {
+        printf("2: CUDA error copying to Device: %s\n", cudaGetErrorString(err));
+    }
+
+
+    cudaEventRecord(E5, 0);
+    cudaEventSynchronize(E5);
+
+    Kernel01<<<dimGrid, dimBlock>>>(channels, d_K2, d_matrix_orig2, width, height, d_matrix_filt2);
+
+    cudaEventRecord(E6, 0);
+    cudaEventSynchronize(E6);
+
+    if (cudaSuccess != cudaGetLastError())
+        printf("3: CUDA error at kernel exec: %s\n", cudaGetErrorString(cudaGetLastError()));
+    err = cudaMemcpy(h_matrix_filt2, d_matrix_filt2, numBytesFilt, cudaMemcpyDeviceToHost);
+    if (err!=cudaSuccess) {
+        printf("4: CUDA error copying to Host: %s\n", cudaGetErrorString(err));
+    }
+
+    cudaFree(d_K2);
+    cudaFree(d_matrix_orig2);
+    cudaFree(d_matrix_filt2);
+
+    cudaEventRecord(E7, 0);
+    cudaEventSynchronize(E7);
+
+    cudaEventElapsedTime(&TiempoTotalCuda2,  E4, E7);
+    cudaEventElapsedTime(&TiempoKernelCuda2, E5, E6);
+    cudaEventDestroy(E4); cudaEventDestroy(E5); cudaEventDestroy(E6); cudaEventDestroy(E7);
+
     printf("Dimensiones: %dx%d\n", N, M);
     printf("nThreads: %dx%d (%d)\n", nThreads, nThreads, nThreads * nThreads);
     printf("nBlocks: %dx%d (%d)\n", nBlocksN, nBlocksM, nBlocksN*nBlocksM);
     printf("Tiempo Secuencial: %4.6f milseg\n", t2-t1);
     printf("Tiempo Paralelo Global con Kernel01: %4.6f milseg\n", TiempoTotalCuda1);
     printf("Tiempo Paralelo Kernel01: %4.6f milseg\n", TiempoKernelCuda1);
+    printf("Tiempo Paralelo Global con Kernel02: %4.6f milseg\n", TiempoTotalCuda2);
+    printf("Tiempo Paralelo Kernel02: %4.6f milseg\n", TiempoKernelCuda2);
 
     //Size of the problem?
     //printf("Rendimiento Paralelo Global con Kernel01: %4.2f GFLOPS\n", (2.0 * (float) N * (float) N * (float) N) / (1000000.0 * TiempoTotalCuda1));
@@ -167,9 +250,13 @@ int main (int argc, char *argv[])
     //Test
     stbi_write_jpg("SEQ.jpg", width, height, STBI_rgb, matrix_filt, 255);
     stbi_write_jpg("CUDA1.jpg", width, height, STBI_rgb, h_matrix_filt, 255);
+    stbi_write_jpg("CUDA2.jpg", width, height, STBI_rgb, h_matrix_filt2, 255);
+
 
     free(h_matrix_orig);
+    free(h_matrix_orig2);
     free(h_matrix_filt);
+    free(h_matrix_filt2);
     free(matrix_filt);
 
 
